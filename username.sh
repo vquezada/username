8bf41c2d920f8e3daeb4b530b0ae2fbb2fa01a8f
“Venezia <vquezada@chapman.edu>
2019-11-10 21:20:25 -0800

    woo we did it

--- username.sh
+++ username.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
-# zip.sh
-# author
-echo "Enter a five-digit ZIP code: "
+# username.sh
+# Venezia Quezada
+echo "The only characters that can be used are: "
+echo "lower case letters, digits, and the underscore character"
+echo "It must start with a lower case letter"
+echo "It must contain at least three but no more than 12 characters"
 read ZIP
-while echo "$ZIP" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+while echo "$ZIP" | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read ZIP
+        echo "You must enter a valid password!"
+        echo "Try again. Enter password: "
+        read ZIP
 done
 echo "Thank you"
